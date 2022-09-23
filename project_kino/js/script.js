5252a66d3a3efd5c85f1ba35a90125e15d8e0f0c
pokemzed <60814385+pokemzed@users.noreply.github.com>
2022-09-23 11:45:16 +0300

    project_kino_day2

---
+++ project_kino/js/script.js
@@ -0,0 +1,8 @@
+// const buttons = document.querySelectorAll('.header__nav a')
+// const win = window.open()
+
+// buttons.forEach((element) => {
+//     element.addEventListener('click', () => {
+//         window.open();
+//     })
+// })
